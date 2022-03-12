03c1a0b5ddf9878245b69e156f896a0ce3aff469
hugosenari <hugosenari@gmail.com>
2022-03-12 02:15:39 -0300

    chore: re index faststreams

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nearly zero-overhead input/output streams for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."faststreams-master".url = "path:./master";
+  inputs."faststreams-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."faststreams-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
